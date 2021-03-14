c1dcc8ec6658f656390732e5a93f4da187c2a3fc
unknown <mimen4070@gmail.com>
2021-03-14 13:17:30 +0100

    clean code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import os 
+import os
 import numpy as np
 import pandas as pd
 from utils import *
@@ -12,45 +12,69 @@ DATA_FOLDER = "machine-learning-with-kernel-methods-2021"
 TRAIN_FILE = "Xtr{}.csv"
 LABEL_FILE = "Ytr{}.csv"
 TEST_FILE = "Xte{}.csv"
-N = 3 # number of datasets
-    
+N = 3  # number of datasets
+
+baselines = {
+    "ridge": kernelRidge,
+    "svm": kernelSVM,
+}
+
 
 def main(args):
-    # model parameters
-    features = args.features.lower()
-    kernel = args.kernel.lower()
-    baseline = args.baseline.lower()
-    k = int(args.k)
-
-    if baseline == "ridge":
-        clf = kernelRidge(kernel)
-    
-    name = baseline+"_"+kernel+"_"+str(k)+"_"+features
+    clf = baselines[args.baseline.lower()](
+        kernel_type=args.kernel.lower(), d=args.d, sigma=args.sigma, c=args.c
+    )
 
     index = []
     pred = []
     for i in range(N):
         xtrain, ytrain, xtest, ids = load_file(i)
-        xtrain, xte = seqToSpec(xtrain, k), seqToSpec(xtest, k)
+
+        feature_type = args.features.lower()
+        if feature_type == "spectrum":
+            xtrain = seqToSpec(xtrain, args.k)
+            xte = seqToSpec(xtest, args.k)
+
         size = int(0.7 * xtrain.shape[0])
-        xtr, xval, ytr, yval = xtrain[:size], xtrain[size:], ytrain[:size], ytrain[size:]
+        xtr, xval, ytr, yval = (
+            xtrain[:size],
+            xtrain[size:],
+            ytrain[:size],
+            ytrain[size:],
+        )
+
         _ = clf.fit(xtr, ytr)
         predval = clf.predict(xtr, xval)
-        predval = threshold(predval)
         print(confusion_matrix(yval, predval))
-        
         ytest = clf.predict(xtr, xte)
-        ytest = threshold(ytest)
+
         index.extend(ids)
         pred.extend(ytest)
+
+    name = args.features.lower() + "_k_" + str(args.k) + clf.type
     save_file(index, pred, name=name)
-    return 1
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--features",  choices=["spectrum", "mismatch", "substring"], default="spectrum")
-    parser.add_argument("--baseline",  choices=["ridge", "logistic", "svm"], default="ridge")
-    parser.add_argument("--kernel",  choices=["linear", "polynomial", "gaussian", "rbf"], default="linear")
-    parser.add_argument("--k",  choices=['3', '4', '5', '6'], default='3')
+    parser.add_argument(
+        "--features",
+        type=str,
+        choices=["spectrum", "mismatch", "substring"],
+        default="spectrum",
+    )
+    parser.add_argument(
+        "--baseline", type=str, choices=["ridge", "logistic", "svm"], default="ridge"
+    )
+    parser.add_argument(
+        "--kernel",
+        type=str,
+        choices=["linear", "polynomial", "gaussian"],
+        default="linear",
+    )
+    parser.add_argument("--k", type=int, default=3)
+    parser.add_argument("--c", type=float, default=0.01)
+    parser.add_argument("--sigma", type=float, default=1)
+    parser.add_argument("--d", type=int, default=2)
     args = parser.parse_args()
     main(args)
